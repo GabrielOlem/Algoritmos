3ff8e1c2bab3fb975a33df81801eb37ff47fd345
GabrielOlem <gme@cin.ufpe.br>
2019-06-07 22:21:35 -0300

    Fui em 0,17s

--- livrariaonline2.cpp
+++ livrariaonline2.cpp
@@ -3,11 +3,9 @@
 using namespace std;
 
 struct aresta{
-    int u;
     int v;
     int custo;
     int distancia;
-    int coeficiente;
 };
 
 struct pedido{
@@ -20,9 +18,8 @@ struct disti{
     int i;
 };
 
-vector<vector<aresta> > grafo;
+
 int **estoque;
-int **certificados;
 bool verifica(pedido *pedidos, int npedidos, int local){
     for(int i=0; i<npedidos; i++){
         if(pedidos[i].qtd > estoque[local][pedidos[i].livro]){
@@ -49,21 +46,7 @@ bool agoravai(aresta a, aresta b){
     return a.v < b.v;
 }
 vector<aresta> h;
-void bubble_up(int i){
-    int p = floor((i - 1)/2);
-    while(i > 0 && agoravai(h[i], h[p])){
-        aresta aux = h[i];
-        h[i] = h[p];
-        h[p] = aux;
-        i = p;
-        p = floor((i - 1)/2);
-    }
-}
 
-void heap_insert(aresta valor){
-    h.push_back(valor);
-    bubble_up(h.size() - 1);
-}
 
 void heapify(int index){
     int r = 2*index + 2;
@@ -85,14 +68,6 @@ void heapify(int index){
         heapify(m);
     }
 }
-
-void heap_extract(){
-    h.pop_back();
-    aresta aux = h[0];
-    h[0] = h[h.size()];
-    h[h.size()] = aux;
-    heapify(0);
-}
 bool compare(disti a, disti b){
     return a.d < b.d;
 }
@@ -103,28 +78,16 @@ int main(){
     int v, e, b;
     cin >> v >> e >> b;
     estoque = new int*[v];
-    certificados = new int*[v];
-    grafo.resize(v);
-    for(int i=0; i<v; i++){
-        certificados[i] = new int[v];
-        for(int j=0; j<v; j++){
-            certificados[i][j] = -1;
-        }
-    }
+    vector<aresta> grafo[v];
     for(int i=0; i<e; i++){
         int x, y;
         cin >> x >> y;
-        aresta a;
-        cin >> a.distancia >> a.coeficiente;
-        a.custo = floor((a.distancia*(100 + a.coeficiente))/100);
-        certificados[x][y] = a.coeficiente;
-        certificados[y][x] = a.coeficiente;
-        a.u = x;
-        a.v = y;
-        grafo[x].push_back(a);
-        a.v = x;
-        a.u = y;
-        grafo[y].push_back(a);
+        int coe;
+        int dis;
+        cin >> dis >> coe;
+        int custo = floor((dis*(100 + coe))/100);
+        grafo[x].push_back({y, custo, dis});
+        grafo[y].push_back({x, custo, dis});
     }
     for(int i=0; i<v; i++){
         estoque[i] = new int[b];
@@ -133,12 +96,12 @@ int main(){
         }
     }
 
-    string oper;
+    char oper[10];
 
-    while(oper != "END"){
+    while(oper[0] != 'E'){
         cin >> oper;
-        if(oper != "END"){
-            if(oper == "ORD"){
+        if(oper[0] != 'E'){
+            if(oper[0] == 'O'){
                 int s, npedidos;
                 cin >> s >> npedidos;
                 pedido livros[npedidos];
@@ -150,10 +113,12 @@ int main(){
                     }
                 }
                 bool temEmAlguem = 1;
-                for(int i=0; i<v; i++){
-                    if(verifica2(livros, npedidos, i)){
-                       temEmAlguem = 0;
-                       break;
+                if(!foi){
+                    for(int i=0; i<v; i++){
+                        if(verifica2(livros, npedidos, i)){
+                        temEmAlguem = 0;
+                        break;
+                        }
                     }
                 }
                 if(foi){
@@ -174,24 +139,31 @@ int main(){
                     }
                     h.clear();
                     dist[s].d = 0;
-                    heap_insert({0,s,0,0,0});
+                    h.push_back({s,0,0});
+                    int i = h.size() - 1;
+                    int p = floor((i - 1)/2);
+                    while(i > 0 && agoravai(h[i], h[p])){
+                        aresta aux = h[i];
+                        h[i] = h[p];
+                        h[p] = aux;
+                        i = p;
+                        p = floor((i - 1)/2);
+                    }
                     for(int ha=0; ha<v; ha++){
                         if(h.size() == 0){
                             break;
                         }
                         aresta removido = h[0];
-                        heap_extract();
+                        h.pop_back();
+                        aresta aux = h[0];
+                        h[0] = h[h.size()];
+                        h[h.size()] = aux;
+                        heapify(0);
                         if(dist[removido.v].d < removido.custo){
                             continue;
                         }
                         for(int e=0; e<grafo[removido.v].size(); e++){
                             int para = grafo[removido.v][e].v;
-                            if(grafo[removido.v][e].coeficiente != certificados[removido.v][grafo[removido.v][e].v] && certificados[removido.v][grafo[removido.v][e].v] != -1){
-
-                                grafo[removido.v][e].coeficiente = certificados[removido.v][grafo[removido.v][e].v];
-                                grafo[removido.v][e].custo = grafo[removido.v][e].distancia*(100 + certificados[removido.v][grafo[removido.v][e].v])/100;
-                        
-                            }
                             if(dist[para].d > removido.custo + grafo[removido.v][e].custo){
 
                                 dist[para].d = removido.custo + grafo[removido.v][e].custo;
@@ -200,8 +172,16 @@ int main(){
                                 aresta a;
                                 a.v = para;
                                 a.custo = dist[para].d;
-
-                                heap_insert(a);
+                                h.push_back(a);
+                                int i = h.size() - 1;
+                                int p = floor((i - 1)/2);
+                                while(i > 0 && agoravai(h[i], h[p])){
+                                    aresta aux = h[i];
+                                    h[i] = h[p];
+                                    h[p] = aux;
+                                    i = p;
+                                    p = floor((i - 1)/2);
+                                }
                             }
                         }
                     }
@@ -232,13 +212,23 @@ int main(){
                   cout << "OOS\n";
                 }
             }
-            else if(oper == "UPD"){
+            else if(oper[0] == 'U'){
                 int x, y, w;
                 cin >> x >> y >> w;
-                certificados[x][y] = w;
-                certificados[y][x] = w;
+                for(int i=0; i<grafo[x].size(); i++){
+                    if(grafo[x][i].v == y){
+                        grafo[x][i].custo = grafo[x][i].distancia*(100 + w)/100;
+                        break;
+                    }
+                }
+                for(int i=0; i<grafo[y].size(); i++){
+                    if(grafo[y][i].v == x){
+                        grafo[y][i].custo = grafo[y][i].distancia*(100 + w)/100;
+                        break;
+                    }
+                }
             }
-            else if(oper == "STK"){
+            else{
                 int x, i, q;
                 cin >> x >> i >> q;
                 estoque[x][i] += q;
